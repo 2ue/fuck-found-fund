fe176dfc417158841803a7724ef3abcc16c241b2
yuanfeijie <yuanfeijie@tencent.com>
2021-01-06 17:54:04 +0800

    update

--- src/utils/fundv2.js
+++ src/utils/fundv2.js
@@ -26,6 +26,11 @@ const invoteMinTime = 7;
 const targetDeficit = -0.1;
 const deficitMinTime = 15;
 
+const DEFAULT_PROPS = {
+    price: 'price',
+    time: 'timer'
+}
+
 const opEnums = {
     sell: '卖出(手动)',
     buy: '买进(手动)',
@@ -47,50 +52,8 @@ function isBlankVar(param) {
 }
 
 class TradingModel {
-    constructor(options = {}, data = []) {
-        this.priceKey = options.priceKey || 'price';
-        this.timeKey = options.timeKey || 'time';
-        // 数据
-        this.data = data;
-        // 初始本金
-        this.invote = options.invote || constantInvote;
-        // 持仓金额
-        this.postionInvote = this.invote;
-        // 持仓本金：随着盈利止盈，投入本金越来越少
-        this.inInvote = this.invote;
-        // 总共投入本金：随着不断买进，投入本金越来越多
-        this.totalInvote = this.invote;
-        // 初始单价
-        this.price = 0;
-        // 市场现价
-        this.marketPrice = 0;
-        // 持仓单价，随着每次买卖，单价会进行变换
-        this.nowPrice = 0;
-        // 初始份额
-        this.amount = 0;
-        // 持仓份额
-        this.nowAmount = this.amount;
-
-        // 累计收益
-        this.totalIncome = 0;
-
-        this.fundCode = options.fundCode;
-        // 恒定本金
-        this.constantInvote = options.constantInvote || constantInvote;
-        // 目标收益和亏损率: 暂未考虑时间
-        this.targetIncome = options.targetIncome || targetIncome;
-        this.targetDeficit = options.targetDeficit || targetDeficit;
-
-        // 记录变化过程
-        this.record = [];
-        this.total = {};
-
-        // 处理方式
-        this.dealType = {
-            // 以金额为标准
-            type: 'money',
-            standard: this.constantInvote
-        }
+    constructor(data = [], options = {}) {
+        this.init(data, options);
     }
     /**
      * 初始化：
@@ -98,19 +61,24 @@ class TradingModel {
      * 1.建仓
      */
     init(data, options) {
+
+        // 要回撤历史数据
         this.data = data;
 
-        this.prop = {
-            price: 'price',
-            time: 'time'
+        this.props = {
+            ...DEFAULT_PROPS,
+            ...options.props
         };
 
         // 基金信息
         // 基金名称，CODE等
-        this.fund = this.options.fund;
+        this.fund = options.fund;
 
         // 建仓时间
         this.startTime = '';
+
+        // 操作时间
+        this.operationTime = '';
 
         // invote: 投入本金
         // 初始投入本金：不会发生改变
@@ -162,27 +130,33 @@ class TradingModel {
         // 卖出频次: 必须高于七天才能卖出，减少手续费
         this.sellInterval = 7;
 
-        this.createPosition(options);
+        this.createPosition(options.startInvote);
     }
 
+    // 取出数据中的价格和时间
     getFundData(data = {}) {
+        console.log('eee==>', this.props.price);
         return {
-            price: data[this.prop.price],
-            time: data[this.prop.time]
+            price: data[this.props.price],
+            time: data[this.props.time]
         };
     }
 
-    // 建仓value
+    /**
+     * 建仓value
+     * 直接无脑买入
+     *  */
     createPosition(value) {
+        console.log('startInvote==>', value);
         let data = this.data[0];
-        if (isEmpty(fund)) {
+        if (!value && value < 0) {
             console.log('无建仓数据');
             return;
         };
         const {
             price,
             time
-        } = this.getFundData(fund);
+        } = this.getFundData(data);
         this.startTime = time;
         this.startPrice = price;
         this.startInvote = value;
@@ -213,8 +187,33 @@ class TradingModel {
         const {
             price,
             time
-        } = this.getFundData(fund);
-        this.updateNetValue(value);
+        } = this.getFundData(data);
+        this.totalInvote = count(value + this.totalInvote);
+        const inFactNeedInvote = value - this.netIncome;
+        if (inFactNeedInvote > 0) {
+            this.netIncome = 0;
+            this.netInvote = count(inFactNeedInvote + this.netInvote);
+        } else {
+            this.netIncome = count(-inFactNeedInvote);
+        }
+        this.postionInvote = count(value + this.postionInvote);
+        this.operationTime = time;
+        this.marketPrice = price;
+        const prePositionAmount = this.positionAmount;
+        const prePositionValue = this.positionValue;
+        this.positionAmount = count(value / price + prePositionAmount);
+        this.positionPrice = count((value + prePositionValue) / this.positionAmount);
+        this.positionValue = count(price * prePositionAmount + value);
+
+        console.log('operationTime==>', this.operationTime);
+        console.log('marketPrice==>', this.marketPrice);
+        console.log('totalInvote==>', this.totalInvote);
+        console.log('netInvote==>', this.netInvote);
+        console.log('netIncome==>', this.netIncome);
+        console.log('postionInvote==>', this.postionInvote);
+        console.log('positionAmount==>', this.positionAmount);
+        console.log('positionPrice==>', this.positionPrice);
+        console.log('positionValue==>', this.positionValue);
     }
 
     updateTotalInvote(value) {
@@ -231,7 +230,7 @@ class TradingModel {
             this.netIncome = changeValue;
         } else {
             this.netIncome = 0;
-            this.netInvote = count(this.netInvote - value);
+            this.netInvote = count(this.netInvote - changeValue);
             this.postionInvote = count(this.postionInvote + this.netInvote);
         };
         this.netRate = count(this.netIncome / this.netInvote);
@@ -244,198 +243,232 @@ class TradingModel {
         }
     }
 
-    // --------------------------------OLD---------------------------------------
+    // // 记录操作
+    // pushRecord(type, result) {
+    //     const {
+    //         time,
+    //         rate,
+    //         preAmount,
+    //         changeMoney,
+    //         changeAmout,
+    //         prePrice,
+    //         income
+    //     } = result;
+    //     this.record.push({
+    //         操作类型: opEnums[type],
+    //         时间: time,
+    //         // 初始本金: this.invote,
+    //         持仓金额: count(this.nowAmount * this.marketPrice),
+    //         在投本金: this.inInvote,
+    //         // 初始单价: this.price,
+    //         上期单价: prePrice,
+    //         市场单价: this.marketPrice,
+    //         持仓单价: this.nowPrice,
+    //         // 初始份额: this.amount,
+    //         // 操作前持仓份额
+    //         上期持仓份额: preAmount,
+    //         最新持仓份额: this.nowAmount,
+    //         变动金额: -changeMoney,
+    //         变动份额: -changeAmout,
+    //         收益: income,
+    //         收益率: `${count(rate * 100)}%`,
+    //         累计收益: this.totalIncome,
+    //         累计收益率: `${count(this.totalIncome / this.totalInvote * 100)}%`,
+    //     })
+    // }
 
-    /**
-     * 1.计算数据
-     * 传入的数据应该是有单价
-     * @param {}} item 
-     */
-    checkTrade(item) {
-        const data = this.countData(item);
-        const { rate } = data;
-        if (rate >= this.targetIncome) {
-            this.sell(data);
-        }
-        if (rate <= this.targetDeficit) {
-            this.buy(data);
-        }
-    }
+    // // --------------------------------OLD---------------------------------------
+
+    // /**
+    //  * 1.计算数据
+    //  * 传入的数据应该是有单价
+    //  * @param {}} item 
+    //  */
+    // checkTrade(item) {
+    //     const data = this.countData(item);
+    //     const { rate } = data;
+    //     if (rate >= this.targetIncome) {
+    //         this.sell(data);
+    //     }
+    //     if (rate <= this.targetDeficit) {
+    //         this.buy(data);
+    //     }
+    // }
  
-    // 计算当前单价的数据变动点
-    countData(item, dealType = this.dealType) {
-        const { standard, type } = dealType;
-        const price = item[this.priceKey];
-        const time = item[this.timeKey];
-        const rate = count(price / this.marketPrice - 1, 4);
-        let changeMoney = 0;
-        let changeAmout = 0;
+    // // 计算当前单价的数据变动点
+    // countData(item, dealType = this.dealType) {
+    //     const { standard, type } = dealType;
+    //     const price = item[this.priceKey];
+    //     const time = item[this.timeKey];
+    //     const rate = count(price / this.marketPrice - 1, 4);
+    //     let changeMoney = 0;
+    //     let changeAmout = 0;
 
-        const markValue = price * this.nowAmount;
+    //     const markValue = price * this.nowAmount;
 
-        if (type === 'money') {
-            changeMoney = count(markValue - standard);
-            changeAmout = count(changeMoney / price, 4);
-        } else {
-            changeMoney = count(price * (this.nowAmount - standard));
-            changeAmout = count(this.nowAmount - standard, 4);
-        }
-        // if (rate >= this.targetIncome || rate <= this.targetDeficit) {
-        //     console.log(this.nowPrice * this.nowAmount, changeMoney);
-        // }
-        return {
-            time,
-            price,
-            rate,
-            preAmount: this.nowAmount,
-            changeMoney,
-            changeAmout,
-            income: changeMoney,
-            changeInvote: changeMoney,
-            postionInvote: count(this.postionInvote - changeMoney),
-            prePrice: this.nowPrice,
-            // nowPrice: count((this.nowPrice * this.nowAmount - changeMoney) / (this.nowAmount - changeAmout), 4),
-            nowPrice: count((standard - changeMoney) / (this.nowAmount - changeAmout), 4)
-        };
-    }
-    // 更新数据
-    updateData(data) {
-        const {
-            price,
-            changeAmout,
-            changeInvote,
-            nowPrice
-        } = data;
-        // this.inInvote = count(this.inInvote - changeInvote);
-        this.nowAmount = count(this.nowAmount - changeAmout, 4);
-        this.marketPrice = price;
-        this.nowPrice = nowPrice;
-    }
+    //     if (type === 'money') {
+    //         changeMoney = count(markValue - standard);
+    //         changeAmout = count(changeMoney / price, 4);
+    //     } else {
+    //         changeMoney = count(price * (this.nowAmount - standard));
+    //         changeAmout = count(this.nowAmount - standard, 4);
+    //     }
+    //     // if (rate >= this.targetIncome || rate <= this.targetDeficit) {
+    //     //     console.log(this.nowPrice * this.nowAmount, changeMoney);
+    //     // }
+    //     return {
+    //         time,
+    //         price,
+    //         rate,
+    //         preAmount: this.nowAmount,
+    //         changeMoney,
+    //         changeAmout,
+    //         income: changeMoney,
+    //         changeInvote: changeMoney,
+    //         postionInvote: count(this.postionInvote - changeMoney),
+    //         prePrice: this.nowPrice,
+    //         // nowPrice: count((this.nowPrice * this.nowAmount - changeMoney) / (this.nowAmount - changeAmout), 4),
+    //         nowPrice: count((standard - changeMoney) / (this.nowAmount - changeAmout), 4)
+    //     };
+    // }
+    // // 更新数据
+    // updateData(data) {
+    //     const {
+    //         price,
+    //         changeAmout,
+    //         changeInvote,
+    //         nowPrice
+    //     } = data;
+    //     // this.inInvote = count(this.inInvote - changeInvote);
+    //     this.nowAmount = count(this.nowAmount - changeAmout, 4);
+    //     this.marketPrice = price;
+    //     this.nowPrice = nowPrice;
+    // }
 
-    updatetotalInvote(type, data) {
-        const {
-            income,
-            changeInvote
-        } = data;
-        console.log(this.totalIncome, changeInvote);
-        if (type === 'autoSell') {
-            this.totalIncome = count(this.totalIncome + income);
-        } else {
-            const tempIncome = count(this.totalIncome + changeInvote);
-            // 过往盈利，能够支撑再投资
-            if (tempIncome >= 0) {
-                this.totalIncome = count(tempIncome);
-            } else {
-                this.totalInvote = count(this.totalInvote - tempIncome);
-                // console.log(this.totalIncome, changeInvote, tempIncome);
-                this.totalIncome = 0;
-            }
-        }
-    }
+    // updatetotalInvote(type, data) {
+    //     const {
+    //         income,
+    //         changeInvote
+    //     } = data;
+    //     console.log(this.totalIncome, changeInvote);
+    //     if (type === 'autoSell') {
+    //         this.totalIncome = count(this.totalIncome + income);
+    //     } else {
+    //         const tempIncome = count(this.totalIncome + changeInvote);
+    //         // 过往盈利，能够支撑再投资
+    //         if (tempIncome >= 0) {
+    //             this.totalIncome = count(tempIncome);
+    //         } else {
+    //             this.totalInvote = count(this.totalInvote - tempIncome);
+    //             // console.log(this.totalIncome, changeInvote, tempIncome);
+    //             this.totalIncome = 0;
+    //         }
+    //     }
+    // }
 
-    // 记录操作
-    pushRecord(type, result) {
-        const {
-            time,
-            rate,
-            preAmount,
-            changeMoney,
-            changeAmout,
-            prePrice,
-            income
-        } = result;
-        this.record.push({
-            操作类型: opEnums[type],
-            时间: time,
-            // 初始本金: this.invote,
-            持仓金额: count(this.nowAmount * this.marketPrice),
-            在投本金: this.inInvote,
-            // 初始单价: this.price,
-            上期单价: prePrice,
-            市场单价: this.marketPrice,
-            持仓单价: this.nowPrice,
-            // 初始份额: this.amount,
-            // 操作前持仓份额
-            上期持仓份额: preAmount,
-            最新持仓份额: this.nowAmount,
-            变动金额: -changeMoney,
-            变动份额: -changeAmout,
-            收益: income,
-            收益率: `${count(rate * 100)}%`,
-            累计收益: this.totalIncome,
-            累计收益率: `${count(this.totalIncome / this.totalInvote * 100)}%`,
-        })
-    }
+    // // // 记录操作
+    // // pushRecord(type, result) {
+    // //     const {
+    // //         time,
+    // //         rate,
+    // //         preAmount,
+    // //         changeMoney,
+    // //         changeAmout,
+    // //         prePrice,
+    // //         income
+    // //     } = result;
+    // //     this.record.push({
+    // //         操作类型: opEnums[type],
+    // //         时间: time,
+    // //         // 初始本金: this.invote,
+    // //         持仓金额: count(this.nowAmount * this.marketPrice),
+    // //         在投本金: this.inInvote,
+    // //         // 初始单价: this.price,
+    // //         上期单价: prePrice,
+    // //         市场单价: this.marketPrice,
+    // //         持仓单价: this.nowPrice,
+    // //         // 初始份额: this.amount,
+    // //         // 操作前持仓份额
+    // //         上期持仓份额: preAmount,
+    // //         最新持仓份额: this.nowAmount,
+    // //         变动金额: -changeMoney,
+    // //         变动份额: -changeAmout,
+    // //         收益: income,
+    // //         收益率: `${count(rate * 100)}%`,
+    // //         累计收益: this.totalIncome,
+    // //         累计收益率: `${count(this.totalIncome / this.totalInvote * 100)}%`,
+    // //     })
+    // // }
  
-    inTotal() {
-        const totalIncome = this.record.reduce((t, n = {}) => {
-            return count(t + n['收益']);
-        }, 0);
-        this.total = {
-            基金: this.fundCode,
-            恒定持仓金额: this.constantInvote,
-            止盈点设置: this.targetIncome,
-            买进点设置: this.targetDeficit,
-            初始投入: this.invote,
-            总投入: this.totalInvote,
-            总盈利: totalIncome,
-        }
-    }
+    // inTotal() {
+    //     const totalIncome = this.record.reduce((t, n = {}) => {
+    //         return count(t + n['收益']);
+    //     }, 0);
+    //     this.total = {
+    //         基金: this.fundCode,
+    //         恒定持仓金额: this.constantInvote,
+    //         止盈点设置: this.targetIncome,
+    //         买进点设置: this.targetDeficit,
+    //         初始投入: this.invote,
+    //         总投入: this.totalInvote,
+    //         总盈利: totalIncome,
+    //     }
+    // }
 
-    // 卖出
-    sell(data) {
-        this.updateData(data, this.dealType);
-        this.updatetotalInvote('autoSell', data);
-        this.pushRecord('autoSell', data);
-    }
+    // // 卖出
+    // sell(data) {
+    //     this.updateData(data, this.dealType);
+    //     this.updatetotalInvote('autoSell', data);
+    //     this.pushRecord('autoSell', data);
+    // }
 
-    // 买进
-    buy(data) {
-        this.updateData(data);
-        // this.totalInvote = count(this.totalInvote - data.changeMoney);
-        this.updatetotalInvote('autoBuy', data);
-        this.pushRecord('autoBuy', {
-            ...data,
-            income: 0,
-            rate: 0
-        });
-    }
+    // // 买进
+    // // buy(data) {
+    // //     this.updateData(data);
+    // //     // this.totalInvote = count(this.totalInvote - data.changeMoney);
+    // //     this.updatetotalInvote('autoBuy', data);
+    // //     this.pushRecord('autoBuy', {
+    // //         ...data,
+    // //         income: 0,
+    // //         rate: 0
+    // //     });
+    // // }
 
-    // 清仓
-    clearance(item) {
-        if (this.nowAmount <= 0) return;
-        const data = this.countData(item, {
-            type: 'amout',
-            standard: 0
-        });
+    // // 清仓
+    // clearance(item) {
+    //     if (this.nowAmount <= 0) return;
+    //     const data = this.countData(item, {
+    //         type: 'amout',
+    //         standard: 0
+    //     });
 
-        const _data = {
-            ...data,
-            income: count(data.rate * data.price * this.nowAmount),
-            changeInvote: this.inInvote
-        };
-        this.updateData(_data);
-        this.pushRecord('clear', _data);
-    }
+    //     const _data = {
+    //         ...data,
+    //         income: count(data.rate * data.price * this.nowAmount),
+    //         changeInvote: this.inInvote
+    //     };
+    //     this.updateData(_data);
+    //     this.pushRecord('clear', _data);
+    // }
 
     // 执行策略
-    do() {
-        const { data } = this;
-        if (isBlankVar(data)) return;
-        data.forEach((d, index) => {
-            if (index === 0) {
-                const price = d[this.priceKey];
-                console.log(d[this.priceKey]);
-                this.price = price;
-                this.marketPrice = price;
-                this.nowPrice = price;
-                this.amount = count(this.invote / this.price, 4);
-            }
-            this.checkTrade(d);
-        });
-        this.clearance(data[data.length - 1]);
-        this.inTotal();
-    }
+    // do() {
+    //     const { data } = this;
+    //     if (isBlankVar(data)) return;
+    //     data.forEach((d, index) => {
+    //         if (index === 0) {
+    //             const price = d[this.priceKey];
+    //             console.log(d[this.priceKey]);
+    //             this.price = price;
+    //             this.marketPrice = price;
+    //             this.nowPrice = price;
+    //             this.amount = count(this.invote / this.price, 4);
+    //         }
+    //         this.checkTrade(d);
+    //     });
+    //     this.clearance(data[data.length - 1]);
+    //     this.inTotal();
+    // }
 }
 
 const csv = require('csvtojson');
@@ -447,18 +480,21 @@ function run(code) {
     .fromFile(path.join(__dirname, `../data/${code}.csv`))
     .then((json) => {
         // console.log(json);
-        const aa = new TradingModel({
-            priceKey: 'DWJZ',
-            timeKey: 'FSRQ',
-            invote: 10000,
+        const aa = new TradingModel(json, {
+            props: {
+                price: 'DWJZ',
+                time: 'FSRQ',
+            },
+            startInvote: 10000,
             constantInvote: 10000,
             targetIncome: 0.05,
             targetDeficit: -0.01,
-            fundCode: code
-        }, json);
-        aa.do();
-        console.table(aa.record);
-        console.table(aa.total);
+            fundCode: code,
+            fund: '2',
+        });
+        // aa.do();
+        // console.table(aa.record);
+        // console.table(aa.total);
     })
 }
 run(110011);
