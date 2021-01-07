391ced8aab5ee019905301979ae0fc812ec5ca59
yuanfeijie <yuanfeijie@tencent.com>
2021-01-07 14:18:51 +0800

    update

---
+++ build/precache-manifest.6081878b30366ede15ed021e57bab9f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c56389e9499cd398e7555d9ed9b11e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a4bfe372e013bcfa0ae",
+    "url": "/static/css/2.1b8aa636.chunk.css"
+  },
+  {
+    "revision": "4fc74c073186626a2e1a",
+    "url": "/static/css/main.9b59cae8.chunk.css"
+  },
+  {
+    "revision": "2a4bfe372e013bcfa0ae",
+    "url": "/static/js/2.f606b923.chunk.js"
+  },
+  {
+    "revision": "0d2a3a750081d8b5c551e4f4690bf215",
+    "url": "/static/js/2.f606b923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc74c073186626a2e1a",
+    "url": "/static/js/main.dfe671c5.chunk.js"
+  },
+  {
+    "revision": "2647e458ddda71deedae",
+    "url": "/static/js/runtime-main.47031e1b.js"
+  }
+]);
