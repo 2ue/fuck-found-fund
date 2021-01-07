88f1e98589140f9cd5aa04abedec7306cf124f1a
yuanfeijie <yuanfeijie@tencent.com>
2021-01-07 14:24:06 +0800

    update

---
+++ build/precache-manifest.940af53ab3ec0509e7c064cf1023994b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41dc4b8e57ede44a07e2cf5781697b8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a4bfe372e013bcfa0ae",
+    "url": "./static/css/2.1b8aa636.chunk.css"
+  },
+  {
+    "revision": "660d6ba42377b21093b2",
+    "url": "./static/css/main.9b59cae8.chunk.css"
+  },
+  {
+    "revision": "2a4bfe372e013bcfa0ae",
+    "url": "./static/js/2.f606b923.chunk.js"
+  },
+  {
+    "revision": "0d2a3a750081d8b5c551e4f4690bf215",
+    "url": "./static/js/2.f606b923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "660d6ba42377b21093b2",
+    "url": "./static/js/main.975daa4c.chunk.js"
+  },
+  {
+    "revision": "2aca77ffc44dd43e7147",
+    "url": "./static/js/runtime-main.ad3410fe.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6081878b30366ede15ed021e57bab9f2.js"
+  "./precache-manifest.940af53ab3ec0509e7c064cf1023994b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
